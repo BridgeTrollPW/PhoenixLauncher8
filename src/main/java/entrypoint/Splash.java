ffbff0462f323c81c7eb4009d810a0c9caa10ccb
Luis E <lifribjor@live.de>
2019-02-01 20:27:46 +0100

    initial project setup

---
+++ src/main/java/entrypoint/Splash.java
@@ -0,0 +1,4 @@
+package entrypoint;
+
+public class Splash {
+}
---
+++ src/main/java/entrypoint/StartRoutine.java
@@ -0,0 +1,17 @@
+package entrypoint;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class StartRoutine extends Application {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Phoenix Launcher 0.8.0");
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
